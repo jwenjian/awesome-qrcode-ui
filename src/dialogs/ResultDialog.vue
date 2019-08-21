c745c793df81d42f0865dc4c04b920eb94b0ef71
jwenjian <wenjian.jiang@foxmail.com>
2019-08-21 22:36:07 +0800

    download image ok

--- src/dialogs/ResultDialog.vue
+++ src/dialogs/ResultDialog.vue
@@ -21,7 +21,11 @@
 </template>
 
 <script>
-import { get_normal_qrcode, get_picture_qrcode, get_gif_qrcode } from "../api/service";
+import {
+  get_normal_qrcode,
+  get_picture_qrcode,
+  get_gif_qrcode
+} from "../api/service";
 
 export default {
   name: "result-dialog",
@@ -97,7 +101,63 @@ export default {
         this.$emit("done");
       }
     },
-    doDownload() {}
+    doDownload() {
+      let fileSuffix = '.png'
+
+      if (this.qrcode.url.indexOf('image/gif') > 0) {
+        fileSuffix = '.gif'
+      }
+
+      let now = new Date()
+
+      let filename = `awesome-qrcode_${now.getFullYear()}_${now.getMonth()}_${now.getDate()}_${now.getHours()}_${now.getMinutes()}_${now.getSeconds()}${fileSuffix}`
+
+      this.saveAsImg(filename, this.qrcode.url)
+    },
+    saveAsImg(fileName, url) {
+      // from echarts
+      // https://github.com/apache/incubator-echarts/blob/master/src/component/toolbox/feature/SaveAsImage.js
+      const a = document.createElement("a");
+      a.download = fileName;
+      a.href = url;
+      const ua = navigator.userAgent;
+      // IE 11 Trident/7.0; rv:11.0
+      const isIE =
+        ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/.+?rv:(([\d.]+))/);
+      const isEdge = ua.match(/Edge\/([\d.]+)/);
+      // firefox || chrome
+      if (typeof MouseEvent === "function" && !isIE && !isEdge) {
+        var evt = new MouseEvent("click", {
+          view: window,
+          bubbles: true,
+          cancelable: false
+        });
+        a.dispatchEvent(evt);
+      } else {
+        if (window.navigator.msSaveOrOpenBlob) {
+          var bstr = atob(url.split(",")[1]);
+          var n = bstr.length;
+          var u8arr = new Uint8Array(n);
+          while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+          }
+          var blob = new Blob([u8arr]);
+          window.navigator.msSaveOrOpenBlob(blob, fileName);
+        } else {
+          var html =
+            "" +
+            '<body style="margin:0;">' +
+            '<img src="' +
+            url +
+            '" style="max-width:100%;" title="' +
+            fileName +
+            '" />' +
+            "</body>";
+          var tab = window.open();
+          tab.document.write(html);
+        }
+      }
+    }
   }
 };
 </script>
